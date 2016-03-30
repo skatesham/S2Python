888f4854e7182fc9a59d07d7340a64aa8e9a75c0
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-03-30 13:20:13 -0300

    Função Ordenação - Insertion Sort
    Função Ordenação - Insertion Sort
    UUU

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -2,21 +2,22 @@ import unittest
 
 
 def insertion_sort(seq):
-    flag = 0
-    if not seq:
+    '''
+    Função tem como entrada uma lista de numeros e retorna
+    a mesma lista organizada, faz iteração de n-1 em n vezes.
+    Função tem tempo em O(n**2) e espaço O(1)
+    :param seq: lista como numeros
+    :return seq: retorna mesma lista organizada por valor
+    '''
+    if len(seq) <= 1:
         return seq
-    if len(seq) == 1:
-        return seq
-    for i in range(1, len(seq)):
-        count = i
-        while count == 0:
-            count -= 1
-            if min(seq[i], seq[count]):
-                flag = 1
-            elif flag == 0:
-                break
-            else:
-                seq[i], seq[count] = seq[count], seq[i]
+    local = 1
+    for i in range(1, len(seq)+1):
+        local = i - 1;
+        while local != 0:
+            if seq[local] < seq[local-1]:
+                seq[local], seq[local-1] = seq[local-1], seq[local]
+            local -= 1
     return seq
 
 
@@ -30,7 +31,7 @@ class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_binaria(self):
         self.assertListEqual([1, 2], insertion_sort([2, 1]))
 
-    def teste_lista_binaria(self):
+    def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], insertion_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
 
 
