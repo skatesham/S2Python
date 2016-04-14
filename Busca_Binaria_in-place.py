736847e6acd5733d44db992766d83eff0e9c9f13
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-04-13 22:14:41 -0300

    Busca Binária
    Deve retornar o índice onde o elemento deveriar ser inserido em lista
    ordenada
    Programa Tem caracteristicas de Tempo em O(N log(N)) e Espaço em O(1)

---
+++ Busca_Binaria_in-place.py
@@ -0,0 +1,49 @@
+def busca_binaria(seq, procurado):
+    """
+    Deve retornar o índice onde o elemento deveriar ser inserido em lista ordenada
+    Programa Tem caracteristicas de Tempo em O(N log(N)) e Espaço em O(1)
+    :param procurado: elemento a ser procurado
+    :param seq: sequencia a ser pesquisada
+    :return: int
+    """
+    inicio = 0
+    final = len(seq)
+    while final > inicio:
+        meio = (inicio + final) // 2
+        if procurado <= seq[meio]:
+            final = meio
+        else:
+            inicio = meio + 1
+    return final
+
+
+import unittest
+
+
+class BuscaBinariaTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertEqual(0, busca_binaria([], 1))
+        self.assertEqual(0, busca_binaria([], 2))
+        self.assertEqual(0, busca_binaria([], 3))
+
+    def teste_lista_unitaria(self):
+        self.assertEqual(0, busca_binaria([1], 0))
+        self.assertEqual(0, busca_binaria([1], 1))
+        self.assertEqual(1, busca_binaria([1], 2))
+        self.assertEqual(1, busca_binaria([1], 3))
+        self.assertEqual(1, busca_binaria([1], 4))
+
+    def teste_lista_nao_unitaria(self):
+        lista = list(range(10))
+        self.assertEqual(0, busca_binaria(lista, -2))
+        self.assertEqual(0, busca_binaria(lista, -1))
+        for i in lista:
+            self.assertEqual(i, busca_binaria(lista, i))
+        self.assertEqual(10, busca_binaria(lista, 10))
+        self.assertEqual(10, busca_binaria(lista, 11))
+        self.assertEqual(10, busca_binaria(lista, 12))
+
+    def teste_lista_elementos_repetidos(self):
+        lista = [1, 1, 1, 2, 2, 2]
+        self.assertEqual(0, busca_binaria(lista, 1))
+        self.assertEqual(3, busca_binaria(lista, 2))
